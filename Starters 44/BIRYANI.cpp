977f92f43716e3e5429a0e1700756bf0b3eaea42
Anubhav <72249059+Anubhavdevv@users.noreply.github.com>
2022-06-23 22:35:20 +0530

    Create BIRYANI.cpp

---
+++ Starters 44/BIRYANI.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int a, b;
+	    cin >> a >> b;
+	    int mul = 0;
+	    mul = a*b;
+	    cout << mul << endl;
+	}
+	return 0;
+}
