38cada206e59bb8cf718234d4839d6ab8145cb18
Anubhav <72249059+Anubhavdevv@users.noreply.github.com>
2022-07-14 02:29:53 +0530

    Create chessTime.cpp

---
+++ starters47/chessTime.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    int div = (n*60)/20;
+	    cout << div << endl;
+	}
+	return 0;
+}
